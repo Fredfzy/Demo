43e843723cd079ca791d480b7b7bd318f70b2cd6
fengyan <18903766550@163.com>
2018-11-02 17:39:21 +0800

    自定义下拉

---
+++ vue/gsdemo/custom-list.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul class="list">
+    <li v-for="item in list" @click="selectValueHandle(item)">{{item}}</li>
+  </ul>
+</template>
+
+<script>
+    export default {
+      name: "custom-list",
+      props: ["list"],
+      methods: {
+        selectValueHandle(item) {
+          //在子组件中触发，需要一个自定义事件
+          // Key值 是父级的自定义事件
+          this.$emit("receive", item);
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue/gsdemo/custom-select.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="wrap">
+    <section class="searchIpt clearFix">
+      <section class="clearFix">
+        <input type="text" class="keyWord" :value="val"  @click="selectShow=!selectShow"/>
+        <!--<input type="button"  :value="btnValue"/>-->
+        <span></span>
+      </section>
+      <custom_list v-show="selectShow" :list="list"  @receive="changeValueHandle"></custom_list>
+    </section>
+  </section>
+</template>
+
+<script>
+  import custom_list from '@/components/zuzengData/zuzengCommon/custom-list'
+    export default {
+      name: "custom-select",
+      components:{
+        custom_list
+      },
+      data(){
+        return {
+          selectShow: false,
+          val: ""
+        }
+      },
+      props: ["btnValue", "list"],
+      methods: {
+        changeValueHandle(value) {
+          this.val = value;  //将选中的值赋给指定的input
+          this.selectShow = false;  //隐藏下拉框
+        }
+      }
+    }
+</script>
+
+<style scoped>
+.clearFix input{
+  width: 257px;
+  height: 60px;
+  border-radius: 4px;
+  border: solid 2px #293745;
+}
+</style>
